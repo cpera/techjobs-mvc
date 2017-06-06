adcc4af2b6590066c9c71e084d179658a31ff3f3
Chaitanya Peramsetty <chaitanya.peramsetty@gmail.com>
2017-06-05 19:34:58 -0500

    search all by value functionality added..

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -27,7 +27,18 @@ public class SearchController {
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         model.addAttribute("columns", ListController.columnChoices);
         if (searchType.equals("all")) {
-            ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+            ArrayList<HashMap<String, String>> jobs;
+            if (searchTerm.equals(null))
+            {
+                //ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+                jobs = JobData.findAll();
+            }
+            else
+            {
+                    //ArrayList<HashMap<String, String>> jobs = JobData.findAll(searchTerm);
+                    jobs = JobData.findByValue(searchTerm);
+            }
+
             model.addAttribute("title", "All Jobs");
             model.addAttribute("jobsSize", jobs.size() + " Result(s)");
             model.addAttribute("jobs", jobs);
